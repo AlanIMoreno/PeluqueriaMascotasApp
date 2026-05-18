b414124595a4616fef6c340dc97661c39e59a959
ALAN IVAN MORENO <34940156@comunidadort.edu.ar>
2026-05-18 20:21:07 -0300

    Add project files.

---
+++ PeluqueriaMascotasMVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using PeluqueriaMascotasMVC.Models;
+using System.Diagnostics;
+
+namespace PeluqueriaMascotasMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
