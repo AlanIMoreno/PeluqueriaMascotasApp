c5073a9b085241c85b555ff46df31d9099dff6a2
ALAN IVAN MORENO <34940156@comunidadort.edu.ar>
2026-06-08 20:23:02 -0300

    Modificacion enums.

--- PeluqueriaMascotasMVC/Models/TipoMascota.cs
+++ PeluqueriaMascotasMVC/Models/TipoMascota.cs
@@ -2,9 +2,10 @@
 {
     public enum TipoMascota
     {
+        Adesignar = 0,
         Perro = 1,
         Gato = 2,
         Conejo = 3,
-            Adesignar = 4,
+            
     }
 }
--- PeluqueriaMascotasMVC/Models/TipoServicio.cs
+++ PeluqueriaMascotasMVC/Models/TipoServicio.cs
@@ -2,11 +2,12 @@ namespace PeluqueriaMascotasMVC.Models
 {
     public enum TipoServicio
     {
+        Otros = 0,
         Consulta = 1,
         Corte = 2,
         Lavado = 3,
         CorteYLavado = 4,
         Peluqueria = 5,
-        Otros = 6
+
     }
 }
